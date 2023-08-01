61cc3bb2a7c5b2b8d07470ee48e9553857f601ce
Caroline Alves <132011536+Caroli-ne@users.noreply.github.com>
2023-08-01 10:40:35 -0300

    Add files via upload

---
+++ script/bancodedadosbiblioteca.sql
@@ -0,0 +1,68 @@
+/*https://dbdiagram.io/d/64c41ee202bd1c4a5ee1c451*/
+
+CREATE DATABASE IF NOT EXISTS biblioteca; 
+
+/*criação da tabela livro, que contém informações sobre todas as obras disponíveis para leitura na biblioteca*/ 
+CREATE TABLE IF NOT EXISTS `livro` (
+    `id` INT,
+    `titulo` VARCHAR(55),
+    `lancamento` DATE,
+    `numExemplares` INT,
+    `genero` VARCHAR(55),
+    `id_localizacao_fk` INT,
+    `id_autor_fk` INT,
+    `id_editora_fk` INT
+);
+
+/*criação da tabela editora, que contém informações sobre as editoras que publicam os livros*/
+CREATE TABLE IF NOT EXISTS`editora` (
+    `id` INT,
+    `nome` VARCHAR(55), 
+    `cnpj` VARCHAR(14),
+    `telefone` INT(55),
+    `email` VARCHAR(255)
+);
+
+/*criação da tabela leitor, que contém informações dos usuários credenciados para utilizar a biblioteca*/
+CREATE TABLE IF NOT EXISTS `leitor` (
+    `id` INT,
+    `nome` VARCHAR(55),
+    `cpf` VARCHAR(11),
+    `dataNascimento` DATE, 
+    `email` VARCHAR(55)
+);
+
+/*criação de tabela autor, contendo informações dos autores dos livros da biblioteca*/
+CREATE TABLE IF NOT EXISTS `autor` (
+    `id` INT,
+    `nome` VARCHAR(55),
+    `genero` VARCHAR(55),
+    `livrosPublicados` INT,
+    `nacionalidade` VARCHAR(55)
+);
+
+/*criação da tabela localização, contendo informações sobre onde se encontram os livros*/
+CREATE TABLE IF NOT EXISTS`localizacao` (
+    `id` INT,
+    `corredor` INT,
+    `prateleira` INT,
+    `sessao` INT,
+    `andar` VARCHAR(55)
+);
+
+/*criação da tabela emprestimo, com datas de emprestimo e devolução, além das informações do livro pego e do cliente*/
+CREATE TABLE IF NOT EXISTS `emprestimo` (
+    `id` INT,
+    `data_emprestimo` DATE,
+    `data_devolucao` DATE,
+    `id_leitor_fk` INT,
+    `id_livro_fk` INT
+);
+
+/*Chaves estrangeiras */
+
+ALTER TABLE `livro` ADD FOREIGN KEY (`id_localizacao_fk`) REFERENCES `localizacao` (`id`);
+ALTER TABLE `livro` ADD FOREIGN KEY (`id_autor_fk`) REFERENCES `autor` (`id`);
+ALTER TABLE `livro` ADD FOREIGN KEY (`id_editora_fk`) REFERENCES `editora` (`id`);
+ALTER TABLE `emprestimo` ADD FOREIGN KEY (`id_leitor_fk`) REFERENCES `leitor` (`id`);
+ALTER TABLE `emprestimo` ADD FOREIGN KEY (`id_livro_fk`) REFERENCES `livro` (`id`);
---
+++ script/populandobiblioteca.sql
@@ -0,0 +1,51 @@
+USE `biblioteca`
+
+/*populando tabela livros*/
+INSERT INTO biblioteca.livro VALUES
+(1,'O silêncio dos amantes','2008-08-21',3,`romance`,2,1,1),
+(2,'O castelo dos alquimistas','196-03-12',2,'ficção',5,2,2),
+(3,'Jurassic Park','1990-11-20',5,'ficção',4,3,3),
+(4,'O silêncio dos inocentes','1988-05-19','suspense',3,4,4),
+(5,'Alien','1978-03-02',4,'terror',1,5,5);
+
+/*populando tabela editora*/
+INSERT INTO biblioteca.editora VALUES
+(1,'Record','20548932841294','1199443312','editorarecord@gmail.com'),
+(2,'Geração Editorial','29347249301245','123345679','editorageração@hotmail.com'),
+(3,'Aleph','11235813213455','1123581321','editoraaleph@yahoo.com'),
+(4,'Rocco','39482834712549','41998823454','roccoeditora@rocco.com'),
+(5,'Darkside','43904523521457','21993450544','darkside@editora.com');
+
+/*populando tabela leitor*/
+INSERT INTO biblioteca.leitor VALUES
+(1,'Yule Carvalho','05582558524','1998-03-20','yulecar@hotmail.com'),
+(2,'Yuri da Silva','29345813212','2001-05-02','yurisilva@gmail.com'),
+(3,'Jurema Romero','23459103029','1950-03-20','ju-re-ma@yahoo.com'),
+(4,'Romário Junior','22394567814','1998-11-11','romarinho@sasasa.com'),
+(5,'Carla Teixeira','11238405942','1995-09-03','carlahtinha@hotmail.com');
+
+/*populando tabela autor*/
+INSERT INTO biblioteca.autor VALUES
+(1,'Lya Luft','romance',3,'brasileira'),
+(2,'Peter Bartschat','ficção',1,'escocês'),
+(2,'Michael Crichton','ficção',10,'americano'),
+(4,'Thomas Harris','terror',4,'inglês'),
+(5,'Alan Dean','terror',4,'americano');
+
+/*populando tabela localizacao*/
+INSERT INTO biblioteca.localizacao VALUES
+(1,3,4,1,'térreo'),
+(2,4,2,4,'1°'),
+(3,2,7,2,'térreo'),
+(4,2,9,3,'1°'),
+(5,4,1,3,'térreo');
+
+/*populando tabela emprestimo*/
+INSERT INTO biblioteca.emprestimo VALUES
+(1,'2023-08-01','2023-08-15',2,4),
+(2,'2023-07-15','2023-07-31',1,5),
+(3,'2023-07-01','2023-07-15',3,1),
+(4,'2023-06-15','2023-06-30',4,2),
+(5,'2023-06-01','2023-06-15',5,3);
+
+
